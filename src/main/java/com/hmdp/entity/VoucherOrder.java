954fb681a8826648cc4077219d1b4a7f872f6290
Macro-Laplace <810282264@qq.com>
2023-02-28 15:19:07 +0800

    基于redis的验证码登陆

--- src/main/java/com/hmdp/entity/VoucherOrder.java
+++ src/main/java/com/hmdp/entity/VoucherOrder.java
@@ -1,14 +1,13 @@
 package com.hmdp.entity;
 
-import com.baomidou.mybatisplus.annotation.IdType;
-import com.baomidou.mybatisplus.annotation.TableId;
 import com.baomidou.mybatisplus.annotation.TableName;
+import com.baomidou.mybatisplus.annotation.IdType;
+import java.time.LocalDateTime;
+import com.baomidou.mybatisplus.annotation.TableId;
+import java.io.Serializable;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 import lombok.experimental.Accessors;
-
-import java.io.Serializable;
-import java.time.LocalDateTime;
 
 /**
  * <p>
@@ -29,7 +28,7 @@ public class VoucherOrder implements Serializable {
     /**
      * 主键
      */
-    @TableId(value = "id", type = IdType.INPUT)
+    @TableId(value = "id", type = IdType.AUTO)
     private Long id;
 
     /**
@@ -45,12 +44,12 @@ public class VoucherOrder implements Serializable {
     /**
      * 支付方式 1：余额支付；2：支付宝；3：微信
      */
-    private Integer payType;
+    private Boolean payType;
 
     /**
      * 订单状态，1：未支付；2：已支付；3：已核销；4：已取消；5：退款中；6：已退款
      */
-    private Integer status;
+    private Boolean status;
 
     /**
      * 下单时间
---
+++ src/main/java/com/hmdp/utils/LoginInterceptor.java
@@ -0,0 +1,15 @@
+package com.hmdp.utils;
+
+import org.springframework.web.servlet.HandlerInterceptor;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+public class LoginInterceptor implements HandlerInterceptor {
+
+    @Override
+    public boolean preHandle(HttpServletRequest request, HttpServletResponse response, Object handler) throws Exception {
+        // 1.判断是否需要拦截（ThreadLocal中是否有用户）
+        return UserHolder.getUser() != null;
+    }
+}
